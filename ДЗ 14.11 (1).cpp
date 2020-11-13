b95831d582c457b7ba699579b4de32ed32fe29b4
Fordester <73312948+Fordester@users.noreply.github.com>
2020-11-13 20:02:37 +0300

    Create ДЗ 14.11 (1).cpp

---
+++ ДЗ 14.11 (1).cpp
@@ -0,0 +1,34 @@
+/*Вывести на экран значение самого часто повторяющегося элемента массива.
+Если в массиве несколько таких элементов, вывести из них тот, который имеет самый меньший номер.*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int counter;
+    char symbol;
+    vector<int> often;
+    for (int i = 0; i < str.size(); i++) {
+        counter = 0;
+        symbol = str[i];
+        for (int j = 0; j < str.size(); j++) {
+            if (str[j] == symbol) {
+                counter++;
+            }
+        }
+        often.push_back(counter); // i-й элемент массива - то, сколько раз встречается i-й символ строки в самой строке 
+    }
+    int max_count = 0, max_index = 0;
+    for (int i = 0; i < often.size(); i++) {
+        if (often[i] > max_count) {
+            max_count = str[i];
+            max_index = i;
+        }
+    }
+    cout << str[max_index] << endl;
+    return 0;
+}
