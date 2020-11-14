0cd580c0590468630aa15aedb1d7f32b85081eac
Fordester <73312948+Fordester@users.noreply.github.com>
2020-11-14 17:20:40 +0300

    Update and rename Independent work(1).cpp to Самостоятельная работа(1).cpp

--- Самостоятельная работа(1).cpp
+++ Самостоятельная работа(1).cpp
@@ -3,14 +3,14 @@ using namespace std;
 
     int main()
     {
-        int a, b, c, d, f = 0;
+        int a, b, c, d, s = 0;
         cin >> a >> b >> c >> d;
         for (int i = 10000; i < 1000000; i++) {
             if (i % a == b && i % c == d) {
                 cout << i << " ";
-                f = 1;
+                s = 1;
             }
         }
-        if (!f)cout << -1;
+        if (!s)cout << -1;
         return 0;
     }
