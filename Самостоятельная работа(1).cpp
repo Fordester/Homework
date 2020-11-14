e19a7a7eae0b434fc49a7aef5635c0144b9e4675
Fordester <73312948+Fordester@users.noreply.github.com>
2020-11-14 17:22:14 +0300

    Update Самостоятельная работа(1).cpp

--- Самостоятельная работа(1).cpp
+++ Самостоятельная работа(1).cpp
@@ -4,6 +4,7 @@ using namespace std;
     int main()
     {
         int a, b, c, d, s = 0;
+        cout << Введите 4 числа << endl;
         cin >> a >> b >> c >> d;
         for (int i = 10000; i < 1000000; i++) {
             if (i % a == b && i % c == d) {
